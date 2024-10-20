e017abe55ae5fb421f25b5051df1f6fba01683f9
DamianRavinduPeiris <drpeiris3@gmail.com>
2024-10-20 12:43:33 +0530

    Added business rules.

--- src/main/java/com/damian/drools/config/DroolsConfiguration.java
+++ src/main/java/com/damian/drools/config/DroolsConfiguration.java
@@ -1,5 +1,6 @@
 package com.damian.drools.config;
 
+import lombok.extern.log4j.Log4j2;
 import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
@@ -13,18 +14,22 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
+@Log4j2
 public class DroolsConfiguration {
     private final KieServices kieServices = KieServices.Factory.get();
 
     private KieFileSystem getKieFileSystem() {
+        log.info("DroolsConfiguration.getKieFileSystem invoked!");
         KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-        kieFileSystem.write(ResourceFactory.newClassPathResource("offer.drl"));
+        //Below file should be located in the src/resources folder.
+        kieFileSystem.write(ResourceFactory.newClassPathResource("businessRules.drl"));
         return kieFileSystem;
 
     }
 
     @Bean
     public KieContainer getKieContainer() {
+        log.info("DroolsConfiguration.getKieContainer invoked!");
         getKieRepository();
         KieBuilder kb = kieServices.newKieBuilder(getKieFileSystem());
         kb.buildAll();
@@ -34,12 +39,14 @@ public class DroolsConfiguration {
     }
 
     private void getKieRepository() {
+        log.info("DroolsConfiguration.getKieRepository invoked!");
         final KieRepository kieRepository = kieServices.getRepository();
         kieRepository.addKieModule(kieRepository::getDefaultReleaseId);
     }
 
     @Bean
     public KieSession getKieSession()  {
+        log.info("DroolsConfiguration.getKieSession invoked!");
         return getKieContainer().newKieSession();
     }
 }
