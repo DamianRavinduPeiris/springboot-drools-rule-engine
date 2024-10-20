276363141028bfb89fdbd6640b7d29d57e9096f5
DamianRavinduPeiris <drpeiris3@gmail.com>
2024-10-20 12:13:54 +0530

    Initial commit.

---
+++ src/main/java/com/damian/drools/DroolsWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.damian.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsWithSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsWithSpringbootApplication.class, args);
+    }
+
+}
